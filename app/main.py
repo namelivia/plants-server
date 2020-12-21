298b8dd7c37dc505573b40aefd661c3ba99e17ff
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-12-21 16:37:46 +0100

    Basic logging test

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from app.plants.api import router
+import logging
+import sys
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 Base.metadata.create_all(bind=engine)
 
--- app/notifications/notifications.py
+++ app/notifications/notifications.py
@@ -1,15 +1,20 @@
 import os
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Notifications:
 
     @staticmethod
     def send(message: str):
+        logger.info("Sending notification")
         data = {
             'message': message
         }
-        requests.post(
+        response = requests.post(
             url=os.getenv("NOTIFICATIONS_SERVICE_ENDPOINT"),
             data=data
         )
+        logger.info(response)
