7a7108aa701f64047845de33d49e92fe117fd487
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-03 15:57:56 +0100

    Returning a user info stub

--- app/users/api.py
+++ app/users/api.py
@@ -19,6 +19,20 @@ router = APIRouter(
 async def get_current_user(
     x_pomerium_jwt_assertion: Optional[str] = Header(None)
 ):
+    # Bypassing this until the thrid party is fixed.
+    # https://github.com/jpadilla/pyjwt/issues/593
+
+    return {
+        "aud": [
+            "example"
+        ],
+        "email": "user@example.com",
+        "exp": 1237658,
+        "iat": 1237658,
+        "iss": "test.example.com",
+        "nbf": 1237658,
+        "sub": "user",
+    }
     url = os.environ['JWK_ENDPOINT']
     jwks_client = PyJWKClient(url)
     signing_key = jwks_client.get_signing_key_from_jwt(
