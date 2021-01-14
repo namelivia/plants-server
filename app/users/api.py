2d2ff5efe4502640ade6c1ca73fba0a45220f72c
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-14 21:24:15 +0100

    Do not verify the aud field

--- app/users/api.py
+++ app/users/api.py
@@ -16,6 +16,11 @@ async def get_current_user(x_pomerium_jwt_assertion: Optional[str] = Header(None
     jwks_client = PyJWKClient(url)
     signing_key = jwks_client.get_signing_key_from_jwt(x_pomerium_jwt_assertion)
     decoded = jwt.decode(
-        x_pomerium_jwt_assertion, signing_key.key, algorithms=["ES256"]
+        x_pomerium_jwt_assertion,
+        signing_key.key,
+        algorithms=["ES256"],
+        options={
+            "verify_aud": False,
+        },
     )
     return decoded
