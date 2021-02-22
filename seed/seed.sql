5c4c2c0d89168e755bcc80d59b8ff89dde62d945
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-02-22 19:10:42 +0100

    Add seeder

---
+++ seed/seed.sql
@@ -0,0 +1,4 @@
+--Insert some test plants
+INSERT INTO plants (id, name, description, days_until_watering, image, journaling_key, last_watering) VALUES (1, 'Test plant 1', 'This is test plant 1', 7, 'test_image_1', gen_random_uuid(), '2020-11-07 20:19:30.000000');
+INSERT INTO plants (id, name, description, days_until_watering, image, journaling_key, last_watering) VALUES (2, 'Test plant 2', 'This is test plant 2', 7, 'test_image_2', gen_random_uuid(), '2020-11-07 20:19:30.000000');
+INSERT INTO plants (id, name, description, days_until_watering, image, journaling_key, last_watering) VALUES (3, 'Test plant 3', 'This is test plant 3', 7, 'test_image_3', gen_random_uuid(), '2020-11-07 20:19:30.000000');
