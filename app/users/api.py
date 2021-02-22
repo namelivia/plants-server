bb1d84e271691c1c97b5830c804c3c0b15b96169
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-02-22 12:32:19 +0100

    Delegate user identification on microservice

--- app/users/api.py
+++ app/users/api.py
@@ -1,7 +1,5 @@
 from typing import Optional
-import jwt
-import os
-from jwt import PyJWKClient
+from app.user_info.user_info import UserInfo
 from fastapi import APIRouter, Header
 import logging
 
@@ -12,15 +10,7 @@ router = APIRouter(prefix="/users")
 
 @router.get("/me")
 async def get_current_user(x_pomerium_jwt_assertion: Optional[str] = Header(None)):
-    url = os.environ["JWK_ENDPOINT"]
-    jwks_client = PyJWKClient(url)
-    signing_key = jwks_client.get_signing_key_from_jwt(x_pomerium_jwt_assertion)
-    decoded = jwt.decode(
-        x_pomerium_jwt_assertion,
-        signing_key.key,
-        algorithms=["ES256"],
-        options={
-            "verify_aud": False,
-        },
-    )
-    return decoded
+    try:
+        return UserInfo.get_current(x_pomerium_jwt_assertion)
+    except Exception as err:
+        logger.error(f"User info could not be retrieved: {str(err)}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -80,9 +80,21 @@ class TestApp:
         response = client.get("/plants/99")
         assert response.status_code == 404
 
-    @pytest.mark.skip(reason="no longer valid")
-    def test_get_current_user(self, client):
-        response = client.get("/users/me")
+    @patch("app.users.api.UserInfo.get_current")
+    def test_get_current_user(self, m_get_user_info, client):
+        m_get_user_info.return_value = {
+            "aud": ["example"],
+            "email": "user@example.com",
+            "exp": 1237658,
+            "iat": 1237658,
+            "iss": "test.example.com",
+            "nbf": 1237658,
+            "sub": "user",
+            "name": "User Name",
+        }
+        response = client.get(
+            "/users/me", headers={"X-Pomerium-Jwt-Assertion": "jwt_assertion"}
+        )
         assert response.status_code == 200
         assert response.json() == {
             "aud": ["example"],
@@ -92,7 +104,9 @@ class TestApp:
             "iss": "test.example.com",
             "nbf": 1237658,
             "sub": "user",
+            "name": "User Name",
         }
+        m_get_user_info.assert_called_with("jwt_assertion")
 
     def test_get_existing_plant(self, client, database_test_session):
         key = uuid.uuid4()
