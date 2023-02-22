9720648f28a5e5144f0046e8b1881ee96578552b
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-02-22 21:25:33 +0100

    Add logging

--- app/users/api.py
+++ app/users/api.py
@@ -11,6 +11,8 @@ router = APIRouter(prefix="/users")
 @router.get("/me")
 async def get_current_user(x_pomerium_jwt_assertion: Optional[str] = Header(None)):
     try:
-        return UserInfo.get_current(x_pomerium_jwt_assertion)
+        data = UserInfo.get_current(x_pomerium_jwt_assertion)
+        logger.info(f"Current user: {data}")
+        return data
     except Exception as err:
         logger.error(f"User info could not be retrieved: {str(err)}")
